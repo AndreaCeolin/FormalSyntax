4a04dd6bab9a12462d2b53328eaca1ce9b21b53b
Andrea Ceolin <ceolin@sas.upenn.edu>
2021-03-09 09:39:41 +0100

    Update bootstrap.py

--- UPGMA/bootstrap.py
+++ UPGMA/bootstrap.py
@@ -6,7 +6,7 @@ Created on Tue Jan  3 17:30:42 2017
 Computes quasi-bootstrapped distance matrices according to the Jaccard distance ('00' '1?' '?1' '??' ignored)
 Assumes alphabet to be 1,0,?
 
-@authors: anonymous and anonymous
+@authors: Andrea Ceolin and Luca Bortolussi
 """
 
 
